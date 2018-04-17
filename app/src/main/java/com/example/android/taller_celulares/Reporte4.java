ad8f629308ccf9bcbc0673da04bf46f1c2fc4828
Vcastro7 <jairleano1@gmail.com>
2018-04-16 19:32:35 -0500

    vista terminada

---
+++ app/src/main/java/com/example/android/taller_celulares/Reporte4.java
@@ -0,0 +1,13 @@
+package com.example.android.taller_celulares;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reporte4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte4);
+    }
+}
